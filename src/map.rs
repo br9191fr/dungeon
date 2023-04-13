96dde1bf486b459767cd32a47804d81931e32303
Bruno Ricci <br9191fr@gmail.com>
2023-04-13 14:18:26 +0200

    news tests added OK

--- src/map.rs
+++ src/map.rs
@@ -20,6 +20,7 @@ impl Map {
             tiles: vec![TileType::Floor; NUM_TILES],
         }
     }
+    /*
     pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
         ctx.set_active_console(0);
         for y in camera.top_y .. camera.bottom_y {
@@ -50,7 +51,7 @@ impl Map {
             }
         }
     }
-
+    */
     pub fn in_bounds(&self, point : Point) -> bool {
         point.x >= 0 && point.x < SCREEN_WIDTH
             && point.y >= 0 && point.y < SCREEN_HEIGHT
